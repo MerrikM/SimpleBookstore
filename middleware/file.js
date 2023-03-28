3ae9904d7d5e5979c1af2745fab6ac2b24ccb3a8
MerrikM <merrikm.pl03@gmail.com>
2023-03-29 01:08:27 +0500

    comments added

--- middleware/file.js
+++ middleware/file.js
@@ -2,10 +2,10 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination(request, file, callback) {
-        callback(null, 'public/file')
+        callback(null, 'public/file') // Расположение папки, где будут храниться файлы
     },
     filename(request, file, callback) {
-        callback(null, `${Date.now()} - ${file.originalname}`)
+        callback(null, `${Date.now()} - ${file.originalname}`) // Имя файла
     }
 })
 
